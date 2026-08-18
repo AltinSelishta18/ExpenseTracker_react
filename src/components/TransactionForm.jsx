f94cf9e035963ea21d6046605b6a76e804341324
Altin Selishta <AltinSelishta14@gmail.com>
2026-08-18 17:17:22 +0200

    Saving the proces of designing formular for add Transaction (Still working on)

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,6 +1,7 @@
 //This component will handle the formular where we can add and edit an transaction
 import { useContext } from "react"
 import { TransactionsContext } from "../context/TransactionsContext"
+import TransactionFormStyle from "../style/TransactionForm.module.css"
 
 
 function TransactionForm(){
@@ -39,29 +40,37 @@ function TransactionForm(){
     }
 
     return (
-        <form action="" onSubmit={SubmitForm}>
-            <input type="text" name="name" value={formData.name} onChange={handleChange} />
-            <input type="text" name="description" value={formData.description} onChange={handleChange} />
-            <select name="category" value={formData.category} onChange={handleChange} id="">
-                <option value="Select Category">Select Category</option>
-                <option value="Food">Food</option>
-                <option value="Transport">Transport</option>
-                <option value="Shopping">Shopping</option>
-                <option value="Bills">Bills</option>
-                <option value="Entertainment">Entertainment</option>
-                <option value="Health">Health</option>
-                <option value="Education">Education</option>
-                <option value="Subscription">Subscription</option>
-                <option value="Other">Other</option>
-            </select>
-            <select name="type" value={formData.type} onChange={handleChange} id="">
-                <option value="Transaction Type">Transaction Type</option>
-                <option value="Expense">Expense</option>
-                <option value="Income">Income</option>
-            </select>
-            <input type="number" name="amount" value={formData.amount} onChange={handleChange} id="" />
-            <button>Add Transaction</button>
-        </form>
+        <div className={TransactionFormStyle.container}>
+            <div className={TransactionFormStyle.formVisual}>
+                <h1>Form Design</h1>
+            </div>
+            <div className={TransactionFormStyle.formular}>
+                <form action="" onSubmit={SubmitForm}>
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                    <input type="text" name="description" value={formData.description} onChange={handleChange} />
+                    <select name="category" value={formData.category} onChange={handleChange} id="">
+                        <option value="Select Category">Select Category</option>
+                        <option value="Food">Food</option>
+                        <option value="Transport">Transport</option>
+                        <option value="Shopping">Shopping</option>
+                        <option value="Bills">Bills</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Health">Health</option>
+                        <option value="Education">Education</option>
+                        <option value="Subscription">Subscription</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <select name="type" value={formData.type} onChange={handleChange} id="">
+                        <option value="Transaction Type">Transaction Type</option>
+                        <option value="Expense">Expense</option>
+                        <option value="Income">Income</option>
+                    </select>
+                    <input type="number" name="amount" value={formData.amount} onChange={handleChange} id="" />
+                    <button>Add Transaction</button>
+                </form>
+            </div>
+        </div>
+        
     )
 }
 
